7f81aeab3af4fd769004d67e16c55ea617c183bf
cesarmak <31118333+cesarmak@users.noreply.github.com>
2018-07-03 10:20:16 +0800

    Update string.js

--- src/string.js
+++ src/string.js
@@ -6,7 +6,7 @@ import includeIoMixin from './io-mixin';
 
 export class String {
   constructor(maxLength=UnsignedInt.MAX_VALUE) {
-    this._maxLength = 8192;
+    this._maxLength = maxLength;
   }
 
   read(io) {
